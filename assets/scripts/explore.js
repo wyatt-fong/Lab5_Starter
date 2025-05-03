ae6362412eea1872fd7843a426aafc8a0bd9ca68
Wyatt Fong <fongwyatt@gmail.com>
2025-05-03 11:35:42 -0700

    Finished initial functionality of JS file

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,9 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-
+// This function is called when the DOM is fully loaded and ready for manipulation
 function init() {
   // TODO
+    document.querySelector("[value='select']")
+    
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const hornSelect = document.getElementById('horn-select');
+
+    hornSelect.addEventListener('change', function() {
+        const image = document.querySelector('img');
+        const audio = document.querySelector('audio');
+
+        image.src = `assets/images/${hornSelect.value}.svg`;
+        audio.src = `assets/audio/${hornSelect.value}.mp3`;
+    });
+
+    const volumeControl = document.getElementById('volume');
+
+    volumeControl.addEventListener('input', function() {
+        let volume = volumeControl.value;
+        const volImage = document.querySelector('#volume-controls img');
+        if (volume == 0) {
+            volImage.src = 'assets/icons/volume-level-0.svg';
+        } else if (volume < 33) {
+            volImage.src = 'assets/icons/volume-level-1.svg';
+        } else if (volume < 67) {
+            volImage.src = 'assets/icons/volume-level-2.svg';
+        } else {
+            volImage.src = 'assets/icons/volume-level-3.svg';
+        }
+    });
+
+    const playButton = document.querySelector('button');
+    
+    playButton.addEventListener('click', function() {
+        const hornType = hornSelect.value;
+        const audio = document.querySelector('audio');
+
+        if (hornType === 'party-horn' && volumeControl.value == 0) {
+            alert("Please increase the volume to hear the sound.");
+        } else {
+            audio.volume = volumeControl.value / 100;
+            audio.play();
+        }
+    });
 }
