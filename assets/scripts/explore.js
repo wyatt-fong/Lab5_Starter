c29ffc26dd4795ce3f2f046b95ce92c8b1b375eb
Wyatt Fong <fongwyatt@gmail.com>
2025-05-04 11:56:22 -0700

    Finished explore section

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,9 +1,58 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-// This function is called when the DOM is fully loaded and ready for manipulation
+
 function init() {
-  // TODO
-    document.querySelector("[value='select']")
-    
+    const textArea = document.getElementById('text-to-speak');
+    const voiceSelect = document.getElementById('voice-select');
+    const  faceImage = document.querySelector('img');
+    const synth = window.speechSynthesis;
+
+    function populateVoices() {
+        const voices = synth.getVoices();
+        voices.forEach(voice => {
+            const option = document.createElement('option');
+            option.value = voice.name;
+            option.textContent = `${voice.name} (${voice.lang})`;
+            voiceSelect.appendChild(option);
+        });
+    }
+
+    if (synth.onvoiceschanged !== undefined) {
+        synth.onvoiceschanged = populateVoices;
+    } else {
+        populateVoices();
+    }
+
+    const playSoundButton = document.querySelector('button');
+
+    playSoundButton.addEventListener('click', function() {
+        // Trimmed leading and trailing white space to ensure text is present
+        textArea.value = textArea.value.trim();
+        // Added logic to force user to enter text 
+        if (textArea.value === "") {
+            alert("Please enter a value to speak");
+            return;
+        }
+        const utterance = new SpeechSynthesisUtterance(textArea.value);
+        const selectedOption = voiceSelect.selectedOptions[0].value;
+        const voices = synth.getVoices();
+        const selectedVoice = voices.find(voice => voice.name === selectedOption);
+        
+        // Added logic to force the user to pick a voice
+        if (selectedVoice === undefined) {
+            alert("Please select a voice");
+            return;
+        } else {
+            utterance.voice = selectedVoice;
+        }
+        
+        faceImage.src = 'assets/images/smiling-open.png';
+        utterance.addEventListener('end', function() {
+            faceImage.src = 'assets/images/smiling.png';
+        });
+
+        synth.speak(utterance);
+
+    });
 }
